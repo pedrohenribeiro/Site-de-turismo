b5038c9ca019b109f39e452d4890cc6c1541db0f
Pedro Henrique Ribeiro <pedro.ribeiro33@fatec.sp.gov.br>
2023-11-14 23:50:58 -0300

    Inicio

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+function background1(){
+    document.querySelector('body').style.background = "url('../Imagens/paisagem1.jpg') center center / cover";
+}
+function background2(){
+    document.querySelector('body').style.background = "url('../Imagens/paisagem2.jpg') center center / cover";
+}
+function background3(){
+    document.querySelector('body').style.background = "url('../Imagens/paisagem3.jpg') center center / cover";
+}
+function background4(){
+    document.querySelector('body').style.background = "url('../Imagens/paisagem4.jpg') center center / cover";
+}
